87c2ad5ea367a638b4e06caa07c0d2cf53c55a3f
David Desberg <david@daviddesberg.com>
2013-01-25 18:17:45 -0500

    Refactor CharacterTest to use a data provider

---
+++ tests/fixtures/Character/bar-layouts.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    array(
+        chr(255), /* character */
+        array(  /* expected bar layout */
+            array(1, true),
+            array(3, false),
+            array(1, true),
+            array(1, false),
+            array(3, true),
+            array(1, false),
+            array(3, true),
+            array(1, false),
+            array(1, true)
+        )
+    )
+);
